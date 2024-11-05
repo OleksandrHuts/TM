9979c0a11c49c78b3537ef4bfcc51258073e4a15
Oleksandr Huts <oleksandr.huts@valtech.com>
2024-11-05 13:12:04 +0200

    init project

---
+++ JS1/main.js
@@ -0,0 +1,17 @@
+//alert(123)
+// confirm('Are u okay?');
+
+//string -> number
+// +
+
+const currentYear = 2024;
+
+const userAge = parseInt(prompt('How old are you????'));
+
+if (isNaN(userAge)) {
+    alert('Not a Number!');
+} else {
+    alert(currentYear + userAge);
+}
+
+
---
+++ hw_1/main.js
@@ -0,0 +1,36 @@
+//1//
+/*let userName = prompt("Whot is your name?");
+alert( `Hello ${userName}` );*/
+
+//2//
+/*let age = prompt("When were you born?");
+const nowYear = 2024;
+alert( nowYear- age);*/
+
+//3//
+/*let sideSquare = prompt('Enter the length of the side of the square');
+alert(sideSquare * 4);*/
+
+//4//
+/*let distance = prompt('Enter the distance between the two cities');
+let time = prompt('Enter the time you want to get to the city');
+alert(distance / time);*/
+
+//5//
+/*let memory = prompt('Enter the amount of memory of your device');
+let count = memory / 820;
+alert(parseInt(count));*/
+
+//6//
+let money = prompt('How much money do you have?');
+let price = prompt('Enter the price of chocolate');
+
+let count = money / price;
+alert(parseInt(count));
+let a = money - (price * count);
+alert(a);
+
+//7//
+/*let sumDeposit = prompt('Enter the deposit amount');
+let count = (sumDeposit * 2) * 0.05;
+alert(count);*/
