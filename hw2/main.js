00e0c8d3815bd34d0720a93830e39c3005736a28
TetianaMaior <major10061984@gmail.com>
2024-11-06 15:17:51 +0200

    add dz2

--- hw2/main.js
+++ hw2/main.js
@@ -1,6 +1,6 @@
 //1//
 
-let sum = prompt('Enter namber');
+/*let sum = prompt('Enter namber');
 if(sum < 1000 && sum >100 && !isNaN(sum)) {
     alert(sum);
 } else {
@@ -14,6 +14,31 @@ if(a == b || a == c || b == c) {
     alert('Your nambers are the same ');
  } else {
      alert('Your nambers are not the same ');
- }
+ }*/
 
- 
+//2//
+let sum = prompt('Enter yuor sum');
+
+let currency = prompt('Enter yuor currency');
+const valUSD =41;
+const valEUR = 44;
+const valPLN = 10;
+
+
+if(!isNaN(sum) && isNaN(currency)) {
+        if(currency == 'USD') {
+            alert(sum * valUSD);
+        }
+        if(currency == 'EUR') {
+            alert(sum * valEUR);
+        }
+        if(currency == 'PLN') {
+            alert(sum * valPLN);
+        }
+    } else {
+    alert('Enter valid sum or currency')
+}
+
+
+
+
