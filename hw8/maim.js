270723938a2496bfbef70142e0d13ab1bc5910a2
TetianaMaior <major10061984@gmail.com>
2024-12-08 21:44:51 +0200

    hw7

---
+++ hw8/maim.js
@@ -0,0 +1,15 @@
+const time = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    showTime: function (hours, minutes, seconds) {
+        console.log(`${this.hours} : ${this.minutes} : ${this.seconds}`)
+    }
+}
+
+changeMinutes: function(time) {
+    let minute = 
+}
+
+
+
