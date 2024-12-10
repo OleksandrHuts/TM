5a5daa3f4b7319ba88be3fa26668317850a0a4c5
Oleksandr Huts <oleksandr.huts@valtech.com>
2024-12-10 14:49:00 +0200

    time task

--- hw7/main.js
+++ hw7/main.js
@@ -3,7 +3,6 @@ const groceriesList = [
         name: 'poteto',
         amount: 2,
         bought: true,
-        
     },
     {
         name: 'apple',
@@ -15,7 +14,6 @@ const groceriesList = [
         amount: 5,
         bought: false,
     },
-    
     {
         name: 'banana',
         amount: 1,
@@ -27,16 +25,24 @@ function dispayGroceries() {
     groceriesList.sort((a, b) => {
         return a.bought - b.bought
     })
-    groceriesList.forEach((element) => console.log(element));
+    //groceriesList.forEach((element) => console.log(element));
+    console.log(groceriesList);
+    
 }
-dispayGroceries();
+//dispayGroceries();
 
 function addItem(productName, amount) {
-let product = groceriesList.find(element => element.name === productName);
-if(product) {
-    groceriesList.amount = groceriesList.amount + amount
-} else {
-    groceriesList.push(productName, amount)
+    let productIndex = groceriesList.findIndex(element => element.name === productName);
+    
+    if (productIndex >= 0) {
+        groceriesList[productIndex].amount = groceriesList[productIndex].amount + amount
+    } else {
+        const newProduct = {
+            name: productName,
+            amount,
+            bought: false
+        }
+        groceriesList.push(newProduct)
+    }
 }
-}
-addItem('apple', 2);
+addItem('plum', 2);
--- hw8/maim.js
+++ hw8/maim.js
@@ -2,16 +2,23 @@ const time = {
     hours: 0,
     minutes: 0,
     seconds: 0,
-    showTime: function (hours, minutes, seconds) {
-        console.log(`${this.hours} : ${this.minutes} : ${this.seconds}`)
-    }
-
-
-    changeSeconds: function(hours) {
-    const allSecondc = this.hours * 3600 + this.minutes * 60 + this.seconds;
-}
-    chahgeMinutes: function(minutes) {
-        const allMinutes = this.hours * 60 + this.minutes + this.seconds / 60;
+    secondsInDay: 86400,
+    showTime: function (hours = this.hours, minutes = this.minutes, seconds = this.seconds) {
+        console.log(`${hours} : ${minutes} : ${seconds}`)
+    },
+    getAllTimeInSeconds: function() {
+        return this.hours * 3600 + this.minutes * 60 + this.seconds
+    },
+    changeSeconds: function (seconds) {
+        //весь наявний час переведений у секунди
+        const allTimeInSeconds = this.getAllTimeInSeconds() + seconds;
+        this.seconds = allTimeInSeconds;
+    },
+    convertFromSecondsToTime: function(allTimeInSeconds) {
+        //якщо секунд більше ніж 86400 - новий день
+        //якщо секунд більше ніж 3600 і менше ніж 86400 - нова година
+        //якщо секунд більше 60 і менше 3600 - нова хвилина
+        //інакше просто додати секунди
     }
 }
 
