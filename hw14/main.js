16d4cebb53f1b51aacd15d9150e876d9661480c7
Tetiana Maior <Solomaho@ukr.net>
2025-02-12 20:04:06 +0200

    hw141

--- hw14/main.js
+++ hw14/main.js
@@ -1,7 +1,7 @@
 
 function getData() {
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=6de55f9e30c69ef94e7d23b00032b088';
-const cityName = document.querySelector('.wrapper_input').value || '';
+    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=6de55f9e30c69ef94e7d23b00032b088';
+    const cityName = document.querySelector('.wrapper_input').value || '';
 
     fetch(BASE_URL + `&q=${cityName}`, { method: 'GET' })
         .then(response => {
@@ -23,9 +23,11 @@ function genarateGrid(weather) {
         </div>
         <div class="degree">${weather.temp}</div>
         <div class="effects">${weather.main}</div>
+        <div class="box">
         <div class="wind">${weather.speed}</div>
         <div class="precipitation">${weather.all}</div>
         <div class="humidity">${weather.humidity}</div>
+        </div>
     `
     });
     grid.innerHTML = wet;
