e908e38e1e3974b784080724d77708eb2e5b4c78
Tetiana Maior <Solomaho@ukr.net>
2025-03-05 19:29:23 +0200

    12

--- hw14/main.js
+++ hw14/main.js
@@ -1,34 +1,37 @@
-
+const API_KEY = '6de55f9e30c69ef94e7d23b00032b088'
 function getData() {
-    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=6de55f9e30c69ef94e7d23b00032b088';
+   
+   
     const cityName = document.querySelector('.wrapper_input').value || '';
+
+    if(!cityName) return false;
+
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${ API_KEY}`;
 
     fetch(BASE_URL + `&q=${cityName}`, { method: 'GET' })
         .then(response => {
             return response.json();
         })
         .then(res => {
-            genarateGrid(res.weather)
-            console.log(res);
+            genarateGrid(res)
+           
         })
 }
-function genarateGrid(weather) {
+function genarateGrid(res) {
+    const { main, wind } = res;
     const grid = document.querySelector('.wether_grid');
-    let wet = '';
-    weather.forEach(element => {
-        wet = wet + `
+    let wet = `
     <div class="feel">
             <h2>Feels like</h2>
-            <p>${response.main.feels_like}</p>
+            <p>t ${Math.round(main.feels_like)} °C</p>
         </div>
-        <div class="degree">${weather.temp}</div>
-        <div class="effects">${weather.main}</div>
+        <div class="degree">t ${Math.round(main.temp)} °C</div>
+       
         <div class="box">
-        <div class="wind">${weather.speed}</div>
-        <div class="precipitation">${weather.all}</div>
-        <div class="humidity">${weather.humidity}</div>
+        <div class="pressure">${main.pressure}mm</div>
+        <div class="wind">${wind.speed}m/s</div>
+        <div class="humidity">${main.humidity}%</div>
         </div>
     `
-    });
     grid.innerHTML = wet;
 }
